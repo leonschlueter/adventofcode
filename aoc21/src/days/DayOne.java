f34f8d924046394af48f05a6cc6cc44910d19f96
Leon Schlüter <schlueter.leon@hotmail.com>
2021-11-17 16:32:16 +0100

    IO done.

--- aoc21/src/days/DayOne.java
+++ aoc21/src/days/DayOne.java
@@ -1,10 +1,17 @@
 package days;
-
+import util.InputReader;
+import java.util.*;
 public class DayOne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
+		InputReader ir = new InputReader("dayOne.txt");
+		ArrayList<Integer> in = ir.readIntegerList();
+		
+		for(Integer i : in) {
+			System.out.println(i);
+		}
+		
+	}	
 
 }
