3d74ac28a31885b63ece6b2093f90b0d159ba545
leonschlueter <schlueter.leon@hotmail.com>
2021-12-04 16:13:56 +0100

    Day Five prepared.

---
+++ 2021/src/days/DayFive.java
@@ -0,0 +1,22 @@
+package days;
+
+import java.util.ArrayList;
+
+import util.InputReader;
+
+public class DayFive {
+	public static void main(String[] args) {
+		//Read input and return array
+		InputReader ir = new InputReader("dayFive.txt");
+		ArrayList<Integer> in = ir.readIntegerList();
+		
+		
+		//Task A:
+		for (Integer i : in) {
+			System.out.println(i);
+		}
+		
+		//Task B:
+
+	}
+}
