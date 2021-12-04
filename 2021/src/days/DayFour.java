705d0d86a22476a7c34e20f7e83f198fdf7b2150
Leon Schlüter <schlueter.leon@hotmail.com>
2021-12-04 15:54:14 +0100

    Cleaned Day Four.

--- 2021/src/days/DayFour.java
+++ 2021/src/days/DayFour.java
@@ -93,7 +93,6 @@ public class DayFour {
 		return sum;
 	}
 
-// Debug methods:
 	public static int[][][] resetCard(int[][][] arr) {
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr.length; j++) {
@@ -102,7 +101,8 @@ public class DayFour {
 		}
 		return arr;
 	}
-
+	
+	// Debug methods:
 	public static void printCard(int[][][] card, int num) {
 		for (int i = 0; i < card.length; i++) {
 			for (int j = 0; j < card.length; j++) {
