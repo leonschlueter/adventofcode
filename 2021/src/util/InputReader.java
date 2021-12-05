90b82a80b791f11c520bcbcc01588ef04df8f483
Leon Schlüter <schlueter.leon@hotmail.com>
2021-12-05 13:01:42 +0100

    InputReader changed.

--- 2021/src/util/InputReader.java
+++ 2021/src/util/InputReader.java
@@ -62,4 +62,43 @@ public class InputReader {
 
 		return cards;
 	}
+
+	public ArrayList<Line> createLineArray() {
+		ArrayList<Line> lines = new ArrayList<Line>();
+		Scanner s = new Scanner(this.f);
+		while (s.hasNextLine()) {
+			String[] str = s.nextLine().split(" -> ");
+			int[] input = new int[4];
+			for (int i = 0; i < str.length; i++) {
+				String c = str[i];
+				String[] arr = c.split(",");
+
+				input[0 + 2 * i] = Integer.parseInt(arr[0]);
+				input[1 + 2 * i] = Integer.parseInt(arr[1]);
+
+			}
+
+			Point p1 = new Point(input[0], input[1]);
+			Point p2 = new Point(input[2], input[3]);
+			if (p1.x > p2.x) {
+				Line l = new Line(p2, p1);
+				lines.add(l);
+			} else {
+				Line l = new Line(p1, p2);
+				lines.add(l);
+			}
+
+		}
+		s.close();
+		return lines;
+	}
+
+	public void print2Darray(int[][] array) {
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length; j++) {
+				System.out.print(array[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
 }
