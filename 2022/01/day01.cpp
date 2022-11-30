36560cc5fc565b37d21f75b70bdfea23415fe2ce
Leon Schlüter <lschlueter@student.ethz.ch>
2022-11-30 19:47:36 +0100

    Preparation begun.

--- 2022/01/day01.cpp
+++ 2022/01/day01.cpp
@@ -1,10 +1,14 @@
 #include <fstream>     
 #include <iostream>
+#include "../utils/aoc_utils.h"
+
 int main(int argc, char const *argv[])
 {
     if(argc != 2){
         std::cout << "NOT ENOUGH ARGS \n";
     }
     std::cout << argv[1] << "\n";
+    
+
     return 0;
 }
---
+++ 2022/utils/aoc_utils.h
@@ -0,0 +1,57 @@
+#ifndef AOC_INPUT
+#define AOC_INPUT
+
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <iostream>
+
+//Reads input from file and puts every single int into vector
+std::vector<int> get_singleint_input(std::string filename){
+
+    std::ifstream file(filename);
+    int x;
+    std::vector<int> invec;
+
+    while(file >> x){
+        invec.push_back(x);
+    }
+    std::cout << "Data parsing into single vector done."<<'\n';
+    return invec;
+}
+
+//reads input from file and puts it into 2d array
+std::vector<std::vector<int>> get_int_input(std::string filename){
+    std::string line;
+    std::ifstream file(filename);
+    std::vector<std::vector<int>> matrix;
+
+    while(std::getline(file, line, '\n')){
+        std::stringstream ss(line);
+        std::vector<int> row; 
+        int x; 
+        while(ss >> x){
+            row.push_back(x);
+        }
+        matrix.push_back(row);
+    }
+    std::cout << "Data parsing into multivector done."<<'\n';
+    return matrix;
+}
+
+void print_vector(std::vector<int> vec){
+    for(int i : vec){
+        std::cout << i << ' ';
+    }
+    std::cout << '\n';
+    return;
+}
+
+void print_multivector(std::vector<std::vector<int>> vec){
+    for(std::vector<int> v : vec){
+       print_vector(v);
+    }
+   
+    return;
+}
+#endif
